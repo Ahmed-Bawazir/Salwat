1b67df28eab0815f6a4dfdd73d807027afab1d72
4HM3D <4HM3D7759988@gmail.com>
2023-12-15 18:48:15 +0300

    Testing1234

--- service_worker.js
+++ service_worker.js
@@ -2,7 +2,7 @@
 self.addEventListener("install",e=>{
     e.waitUntil(
       caches.open("static").then(cache=>{
-        return cache.addAll(["./",'./icon.png']);
+        return cache.addAll(["./",'./icon192.png']);
       })
     );
   });

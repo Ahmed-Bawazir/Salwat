ed9cc9f12653fb44caea753bd46992b153ffcd80
4Hm3D-PC <4hm3d7759988@gmail.com>
2024-11-02 18:49:36 +0300

    Create new Service Worker

--- service-worker.js
+++ service-worker.js
@@ -8,69 +8,71 @@ let assets = [
   "manifest.json",
   "date.json",
   "icon192.png",
-];
-let assets2 = [
-  "/",
-  "index.html",
-  "style.css",
-  "js.js",
-  "manifest.json",
-  "date.json",
-];
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(nameOfCache).then(function (cache) {
-      for (let asset of assets) {
-        try {
-          cache.add(asset);
-          //console.log(`add ${asset} to cache done`);
-        } catch (err) {
-          //console.log(`There error to add ${asset} to cache :${err}`);
-        }
-      }
-    })
-  );
-});
-//
-self.addEventListener("fetch", (event) => {
-  if (!event.request.url.includes("icon")) {
-    event.respondWith(
-      networkFirst2({
-        request: event.request,
-        fallbackUrl: "/icon192.png",
-      })
-    );
-  }
-});
-const networkFirst2 = async ({ request, fallbackUrl }) => {
- 
-  try {
-    const responseFromNetwork = await fetch(request);
-    if (responseFromNetwork.ok) {
-      //await putInCache(request, responseFromNetwork.clone());
-      //
-      caches.open(nameOfCache).then(function (cache) {
-        for (let asset of assets2) {
-          try {
-            cache.add(asset);
-            //console.log(`add ${asset} to cache done`);
-          } catch (err) {
-            //console.log(`There error to add ${asset} to cache :${err}`);
-          }
-        }
-      });
-      //
-      //console.log("from network");
+];// the cache version gets updated every time there is a new deployment
+const CACHE_VERSION = 10;
+const CURRENT_CACHE = `main-${CACHE_VERSION}`;
 
-      return responseFromNetwork;
-    }
-  } catch (error) {
-    const responseFromCache = await caches.match(request);
-    if (responseFromCache) {
-      return responseFromCache;
-    } else {
-      const fallbackResponse = await caches.match(fallbackUrl);
-      return fallbackResponse;
-    }
-  }
-};
+// these are the routes we are going to cache for offline support
+const cacheFiles = ['/'];
+
+// on activation we clean up the previously registered service workers
+self.addEventListener('activate', evt =>
+  evt.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CURRENT_CACHE) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  )
+);
+
+// on install we download the routes we want to cache for offline
+self.addEventListener('install', evt =>
+  evt.waitUntil(
+    caches.open(CURRENT_CACHE).then(cache => {
+      return cache.addAll(cacheFiles);
+    })
+  )
+);
+
+// fetch the resource from the network
+const fromNetwork = (request, timeout) =>
+  new Promise((fulfill, reject) => {
+    const timeoutId = setTimeout(reject, timeout);
+    fetch(request).then(response => {
+      clearTimeout(timeoutId);
+      fulfill(response);
+      update(request);
+    }, reject);
+  });
+
+// fetch the resource from the browser cache
+const fromCache = request =>
+  caches
+    .open(CURRENT_CACHE)
+    .then(cache =>
+      cache
+        .match(request)
+        .then(matching => matching || cache.match('/offline/'))
+    );
+
+// cache the current page to make it available for offline
+const update = request =>
+  caches
+    .open(CURRENT_CACHE)
+    .then(cache =>
+      fetch(request).then(response => cache.put(request, response))
+    );
+
+// general strategy when making a request (eg if online try to fetch it
+// from the network with a timeout, if something fails serve from cache)
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    fromNetwork(evt.request, 10000).catch(() => fromCache(evt.request))
+  );
+  evt.waitUntil(update(evt.request));
+});
